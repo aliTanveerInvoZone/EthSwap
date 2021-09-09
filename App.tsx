edd9170b8d962964c156fd646c5af6f329a46179
aliTanveerInvoZone <ali.tanveer@invozone.com>
2021-09-09 20:26:26 +0500

    Connected to Rinkeby Live testnet

--- App.tsx
+++ App.tsx
@@ -8,24 +8,29 @@ import {Header} from './src/Components/header';
 import Token from './src/abis/Token.json';
 import EthSwap from './src/abis/EthSwap.json';
 import {Main} from './src/Components/Main';
+import {Loader} from './src/Components/loader';
 
 import HDWalletProvider from '@truffle/hdwallet-provider';
-import {identifier} from '@babel/types';
 
-const MNEMONIC =
-  'split axis exist miracle have input kite stock laugh govern property call';
-const App: React.FC = () => {
-  const [account, setAccount] = React.useState('');
+function App() {
+  const [account, setAccount] = useState('');
   const [ethBalance, setEthBalance] = useState('');
-  const [tokenContract, setTokenContract] = useState({});
+  const [tokenContract, setTokenContract] = useState('');
   const [tokenBalance, setTokenBalance] = useState('');
-  const [ethSwapContract, setEthSwapContract] = useState({});
-  var hdWalletProvider = new HDWalletProvider(
-    MNEMONIC,
-    'wss://ropsten.infura.io/ws/v3/e55d95fe5dfd4191b0ea5769dc6bf515',
-  );
+  const [ethSwapContract, setEthSwapContract] = useState('');
+  const [loader, setLoader] = useState(false);
+  const privateKey = [
+    'e8862a89dcd4a0dba199679f06f76bdb02b9f80c0241a7efde545034d54b12c9',
+  ];
+  var hdWalletProvider = new HDWalletProvider({
+    privateKeys: privateKey,
+    providerOrUrl:
+      'wss://rinkeby.infura.io/ws/v3/b2cd755f7efc4b6090ece72c55f77bdd',
+    numberOfAddresses: 1,
+  });
   var web3 = new Web3(hdWalletProvider);
   useEffect(() => {
+    setLoader(true);
     const init = async () => {
       await loadBlockChainData();
     };
@@ -33,77 +38,103 @@ const App: React.FC = () => {
   }, []);
 
   async function loadBlockChainData() {
-    console.log('web3', web3.eth.accounts.wallet);
     // Init Web3 Block Chain
-
+    console.log('Load Block Data ===> ');
     let accounts = await web3.eth.getAccounts();
-    console.log('Private key of wallet ====> ', accounts);
-    let balance = await web3.eth.getBalance(accounts[0]);
-    console.log('balance', balance);
-    // Fetching the default account
+    console.log('Accounts Address ====> ', accounts);
+    let balance = await web3.eth.getBalance(accounts[0]); // Fetching the default account
+    console.log('Account balance ====>', balance);
     setAccount(accounts[0]);
     setEthBalance(balance);
+
     const networkId = await web3.eth.net.getId();
-    console.log(networkId);
-    const tokenData = Token.networks[networkId];
-    // Init Token Contract
-    if (tokenData) {
-      const token = await new web3.eth.Contract(Token.abi, tokenData.address);
-      setTokenContract(token);
-      console.log('token', token);
-      const tokenBalance = await token.methods.balanceOf(accounts[0]).call();
-      // Fetching Token Balance
+    console.log('Network ID ====>', networkId);
+
+    if (!tokenContract) {
+      const tokenData = Token.networks[networkId];
+      if (tokenData) {
+        const token = await new web3.eth.Contract(Token.abi, tokenData.address);
+        setTokenContract(token);
+        const tokenBalance = await token.methods.balanceOf(accounts[0]).call(); // Fetching Token Balance
+        console.log('tokenBalance', tokenBalance);
+        setTokenBalance(tokenBalance);
+      } else {
+        alert('Token is not Deployed to Network');
+      }
+    } else {
+      const tokenBalance = await tokenContract.methods
+        .balanceOf(accounts[0])
+        .call(); // Fetching Token Balance
       console.log('tokenBalance', tokenBalance);
       setTokenBalance(tokenBalance);
-    } else {
-      alert('Token is not Deployed to Network');
     }
 
-    const ethSwapData = EthSwap.networks[networkId];
-    if (ethSwapData) {
-      const ethSwap = await new web3.eth.Contract(
-        EthSwap.abi,
-        ethSwapData.address,
-      );
-      setEthSwapContract(ethSwap);
-    } else {
-      alert('EthSwap contract not deployed to network');
+    if (!ethSwapContract) {
+      const ethSwapData = EthSwap.networks[networkId];
+      if (ethSwapData) {
+        const ethSwap = await new web3.eth.Contract(
+          EthSwap.abi,
+          ethSwapData.address,
+        );
+        setEthSwapContract(ethSwap);
+      } else {
+        alert('EthSwap contract not deployed to network');
+      }
     }
+    setLoader(false);
   }
 
   async function buyTokens(value: string) {
-    console.log('value', value);
-    console.log('account', account);
+    setLoader(true);
     await ethSwapContract.methods
       .buyTokens()
       .send({value: value, from: account})
       .on('transactionHash', async hash => {
-        if (hash) {
-          console.log('transactionHash', hash);
+        console.log('buyTokens transaction Hash ===>', hash);
+      })
+      .on('confirmation', async (confirmation: number, receipt: any) => {
+        if (confirmation === 0) {
+          setLoader(false);
+          alert('Transaction  Successful ' + receipt.transactionHash);
           await loadBlockChainData();
         }
+      })
+      .on('error', err => {
+        setLoader(false);
+        console.log('Buy Tokens err', err);
       });
   }
   async function sellTokens(value: string) {
+    setLoader(true);
     tokenContract.methods
       .approve(ethSwapContract._address, value)
       .send({from: account})
       .on('transactionHash', async hash => {
         console.log('transactionHash===>', hash);
-        await ethSwapContract.methods
-          .sellTokens(value)
-          .send({from: account})
-          .on('transactionHash', async hash => {
-            console.log('hash', hash);
-            await loadBlockChainData();
-          });
+      })
+      .on('confirmation', async (confirmation: Number, receipt: any) => {
+        if (confirmation === 0) {
+          await ethSwapContract.methods
+            .sellTokens(value)
+            .send({from: account})
+            .on('transactionHash', async hash => {
+              console.log('hash', hash);
+            })
+            .on('confirmation', async (confirmation: Number, receipt: any) => {
+              if (confirmation === 0) {
+                setLoader(false);
+                alert('Transaction Successful' + receipt.transactionHash);
+                await loadBlockChainData();
+              }
+            });
+        }
       });
   }
 
   return (
     <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
       <Header title={'Dapp'} accountAddress={account} />
-
+      <Loader isLoading={loader} />
       <Main
         ethBalance={ethBalance}
         tokenBalance={tokenBalance}
@@ -112,6 +143,6 @@ const App: React.FC = () => {
       />
     </SafeAreaView>
   );
-};
+}
 
 export default App;
--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,10 @@ var HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const MNEMONIC =
   'split axis exist miracle have input kite stock laugh govern property call';
+
+const privateKey = [
+  'e8862a89dcd4a0dba199679f06f76bdb02b9f80c0241a7efde545034d54b12c9',
+];
 module.exports = {
   networks: {
     development: {
@@ -9,15 +13,20 @@ module.exports = {
       port: 7545,
       network_id: '*', // Match any network id
     },
-    ropsten: {
+    rinkeby: {
       provider: function () {
-        return new HDWalletProvider(
-          MNEMONIC,
-          'https://ropsten.infura.io/v3/e55d95fe5dfd4191b0ea5769dc6bf515',
-        );
+        return new HDWalletProvider({
+          privateKeys: privateKey,
+          providerOrUrl:
+            'https://rinkeby.infura.io/v3/b2cd755f7efc4b6090ece72c55f77bdd',
+          numberOfAddresses: 1,
+        });
       },
-      network_id: 3,
-      gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
+      gas: 5500000,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      network_id: 4,
+      skipDryRun: true,
     },
   },
   contracts_directory: './src/contracts/',
