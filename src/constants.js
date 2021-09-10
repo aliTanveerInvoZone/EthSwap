18ca977553957cb39b3a95285aed6543c1d057bc
aliTanveerInvoZone <ali.tanveer@invozone.com>
2021-09-10 12:04:58 +0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ export const PRIVATE_KEYS = [
   'e8862a89dcd4a0dba199679f06f76bdb02b9f80c0241a7efde545034d54b12c9',
 ];
 export const PROVIDER =
-  'wss://rinkeby.infura.io/ws/v3/b2cd755f7efc4b6090ece72c55f77bdd';
+  'wss://rinkeby.infura.io/ws/v3/fed35652d9de4f0c9910748bac12b905';
 export const HTTP_PROVIDER =
-  'https://rinkeby.infura.io/v3/b2cd755f7efc4b6090ece72c55f77bdd';
+  'https://rinkeby.infura.io/v3/fed35652d9de4f0c9910748bac12b905';
